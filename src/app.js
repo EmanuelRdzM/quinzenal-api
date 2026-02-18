d1e43230d6571eaa199a1eba1d505933e3833255
EmanuelRdzM <j.emanuel.rdz.m@hotmail.com>
2026-02-17 23:07:18 -0600

    cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
 import express from 'express';
+import cors from 'cors';
 import routes from './routes/index.js';
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors({
+  origin: [
+    'http://localhost:5173', // Vite default
+  ],
+  credentials: true
+}));
+
 app.use(express.json());
+
 app.use(routes);
 
 app.get('/', (req, res) => {
