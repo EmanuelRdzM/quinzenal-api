5fa650d0c110a582b650f9176a161c8159ae8bc7
Emanuel Rdz <j.emanuel.rdz.m@hotmail.com>
2026-07-31 16:40:21 -0600

    Refactor CORS configuration to allow all origins for development and production

--- src/app.js
+++ src/app.js
@@ -6,11 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: [
-    'http://localhost:5173', // Vite dev
-    'file://', // Electron production
-    'http://localhost:3000' // In case
-  ],
+  origin: true,
   credentials: true
 }));
 
